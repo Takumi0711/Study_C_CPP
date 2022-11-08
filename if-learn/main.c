5f2c0f386914d446d7f620253cb758832683ebdf
Takumi Aoyama <aotaku4062@gmail.com>
2022-11-09 01:30:51 +0900

    if文について

---
+++ if-learn/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main() {
+	int a;
+	printf("1`6‚Ì”’l‚ð“ü—Í:");
+
+	scanf("%d", &a);
+
+	if (a >= 1 && a <= 6) {
+		if (a % 2 == 0) {
+			printf("’š‚Å‚·\n");
+		}
+		else {
+			printf("”¼‚Å‚·\n");
+		}
+	}
+	else {
+		printf("–³Œø‚È”Žš‚Å‚·");
+	}
+
+	
+}
